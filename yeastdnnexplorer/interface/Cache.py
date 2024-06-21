5fa33bbb448eb324e41c30f9f5aafd7dc828d14f
chasem <chasem@wustl.edu>
2024-06-21 17:31:08 -0500

    init implementation of the API classes. Documentation and some testing included. RankResponse is not, and the testing is minimal due to the difficulty of testing futures

---
+++ yeastdnnexplorer/interface/Cache.py
@@ -0,0 +1,30 @@
+import logging
+from typing import Any, List
+
+from cachetools import TTLCache
+
+
+class Cache:
+    """
+    A caching class that uses cachetools for TTL caching with an LRU eviction policy.
+    """
+
+    def __init__(self, maxsize: int = 100, ttl: int = 300):
+        self.ttl_cache = TTLCache(maxsize=maxsize, ttl=ttl)
+        self.logger = logging.getLogger(__name__)
+
+    def get(self, key: str, default: Any = None) -> Any:
+        """Get a value from the cache."""
+        return self.ttl_cache.get(key, default)
+
+    def set(self, key: str, value: Any) -> None:
+        """Set a value in the cache."""
+        self.ttl_cache[key] = value
+
+    def list(self) -> List[str]:
+        """List all keys in the cache."""
+        return list(self.ttl_cache.keys())
+
+    def delete(self, key: str) -> None:
+        """Delete a key from the cache."""
+        self.ttl_cache.pop(key, None)
